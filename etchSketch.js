1b43d3e4ecec0bc3ab4d2065e398af742c7f53cb
Justin Zimmerman <zimmerj271@pm.me>
2022-03-02 08:12:39 -0700

    Added persistent color change to divs on mouseover

--- etchSketch.js
+++ etchSketch.js
@@ -1,18 +1,18 @@
-/*
-2 x 2
-3 x 3
-*/
-const base = 25
-const boxes = base ** 2;
+
+const base = 100; // number of cells per side
+const numCells = base ** 2;  // size of grid -- number of cells in the grid
 const container = document.querySelector('#container'); // select element with id=container
-for(let i=0; i<boxes; i++) {
-    const child = container.appendChild(document.createElement("div")); // append child div to container element, appendChild reference saved in variable child
-    child.className = 'etch'; // assign class name 'etch' to child reference element
-    // container.appendChild(document.createElement("div")).className = 'etch';
+
+// create numCells of child divs and append to container
+// give each new div class 'etch'
+// add a mouseover event listener to each new div and assign another class when event occurs
+for(let i=0; i<numCells; i++) {
+    const cell = container.appendChild(document.createElement("div")); // append child div to container element, appendChild reference saved in variable child
+    cell.className = 'etch'; // assign class name 'etch' to child reference element
+    cell.addEventListener('mouseover', (e) => e.target.classList.add('hover'));
+
+    // container.appendChild(document.createElement("div")).className = 'etch'; // one liner
 }
 
-container.style.gridTemplateColumns = `repeat(${base}, 1fr)`;
-container.style.gridTemplateRows = `repeat(${base}, 1fr)`;
-// container.setAttribute('style', 'display: flex; grid-template-columns: repeat(2, 1fr)');
-// container.setProperty('grid-template-columns', 'repeat(2, 1fr)');
-
+container.style.gridTemplateColumns = `repeat(${base}, 1fr)`; // change CSS grid property inside container to base number of columns
+container.style.gridTemplateRows = `repeat(${base}, 1fr)`; // change CSS grid property inside container to base number of rows.
