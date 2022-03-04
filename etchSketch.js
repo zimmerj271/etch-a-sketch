04da6195c5d8637fe8a40631acbc79c9ae2ba094
Justin Zimmerman <zimmerj271@pm.me>
2022-03-03 23:02:17 -0700

    Added rainbow effect and button to toggle on and off

--- etchSketch.js
+++ etchSketch.js
@@ -1,30 +1,41 @@
+function listener(colorState = false) {
+    const grid = document.querySelectorAll('.cell');    // NodeList of .cell elements
+    // loop through each .cell elements and assign mouseover eventListener
+    grid.forEach(cell => cell.addEventListener('mouseover', (e) => {    // on event of event
+        if(colorState) {
+            cell.style.backgroundColor = makeRainbow();
+        } else {
+            cell.style.backgroundColor = 'black'                        // add background color
+        }
+        
+        console.log(cell);
+    }));
+};
+
+// function that checks the color of the div.
+// if it already has a background defined, then 
+// return that color but darker.
+function checkColor() {
+
+}
+
 // function which creates the grid based on input size to define number of cells per side
-function makeGrid(size = 16) {
+function makeGrid(size = 16, color = false) {
     const numCells = size ** 2;  // size of grid -- number of cells in the grid
     const container = document.querySelector('#container'); // select element with id=container
     for(let i=0; i<numCells; i++) {
         const cell = container.appendChild(document.createElement("div")); // append child div to container element, appendChild reference saved in variable child
         cell.className = 'cell'; // assign class name 'etch' to child reference element
-        let color;
-        rgb ? color = makeRainbiow() : color = "black";
-        // if(rgb) {
-        //     // change background color to rainbow
-        //     color = makeRainbow();
-        // } else {
-        //     // change background color to black
-        //     color = "black";
-        // }
-        cell.addEventListener('mouseover', (e) => {cell.style.backgroundColor = color});
-    
-        // container.appendChild(document.createElement("div")).className = 'etch'; // one liner
     }
     
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`; // change CSS grid property inside container to base number of columns
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`; // change CSS grid property inside container to base number of rows.
+
+    listener(color);
 }
 
 // function to clear grid and prompt for new size
-function clearGrid() {
+function clearGrid(colorState = false) {
     // remove all divs from the grid
     const grid = document.getElementsByClassName('cell');
     while(grid[0]) {
@@ -39,7 +50,7 @@ function clearGrid() {
     while(newSize > 100) {
         newSize = parseInt(window.prompt("Maximum size is 100"));
     }
-    makeGrid(newSize);
+    makeGrid(newSize, colorState);
 }
 
 function randomize() {
@@ -52,19 +63,18 @@ function makeRainbow() {
     return color;
 }
 
-makeGrid();
+// Trigger creation of grid when DOM is loaded
+window.addEventListener('DOMContentLoaded', (e) => {makeGrid()});
+
 
 const reset = document.querySelector('#reset');
-reset.addEventListener('click', (e) => {clearGrid()} );
+reset.addEventListener('click', (e) => {clearGrid(rgb)} );
 
-let rbg = false;
+let rgb = false;
 const rainbow = document.querySelector('#rainbow');
 rainbow.addEventListener('click', (e) => {
-    if(rbg) {
-        rbg = false;
-    } else {
-        rbg = true;
-    }
-    console.log(rbg);
+    rgb ? rgb = false : rgb = true;
+    listener(rgb);
+    console.log(rgb);
 });
 
